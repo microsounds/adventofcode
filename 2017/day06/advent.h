2fff51680b222c0cd3816eb8d6c9e990664c49aa
microsounds <microsounds@users.noreply.github.com>
2017-12-06 10:52:31 -0800

    Readability

--- 2017/day06/advent.h
+++ 2017/day06/advent.h
@@ -26,22 +26,22 @@ struct split *tokenize(char *, const char *);
 void token_free(struct split *);
 
 /* generic vector container */
-#define vector_generic(name, T) \
-	struct name { T *arr; unsigned len, size; }; \
-	struct name *name##_init(void) { \
-		struct name *out = calloc(1, sizeof(struct name)); \
+#define vector_generic(N, T) \
+	struct N { T *arr; unsigned len, size; }; \
+	static struct N *N ## _init(void) { \
+		struct N *out = calloc(1, sizeof(struct N)); \
 		out->size = 128; \
 		out->arr = malloc(sizeof(T) * out->size); \
 		return out; \
 	} \
-	void name##_insert(struct name *self, T insert) { \
+	static void N ## _insert(struct N *self, T insert) { \
 		if (self->len == self->size) { \
 			self->size *= 1.25f; \
 			self->arr = realloc(self->arr, sizeof(T) * self->size); \
 		} \
 		self->arr[self->len++] = insert; \
 	} \
-	void name##_free(struct name *self) { \
+	static void N ## _free(struct N *self) { \
 		free((!self->arr) ? NULL : self->arr); \
 		free((!self) ? NULL : self); \
 	} \
